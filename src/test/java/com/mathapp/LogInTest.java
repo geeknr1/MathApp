4ad2120348900f48a89a751310e1528da10ed330
Alexandru Burbea <alexandru.burbea01@e-uvt.ro>
2024-12-19 23:44:50 +0200

    Test Update

--- src/test/java/com/mathapp/LogInTest.java
+++ src/test/java/com/mathapp/LogInTest.java
@@ -2,12 +2,28 @@ package com.mathapp;
 
 import java.util.logging.Logger;
 
+import org.bson.Document;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class LogInTest{
     private static final Logger logger = Logger.getLogger(LogInTest.class.getName());
+    private MongoDatabase mockDatabase;
+    private MongoCollection<Document> mockCollection;
+
+    @BeforeEach
+    public void setUp(){
+        mockDatabase = mock(MongoDatabase.class);
+        mockCollection = mock(MongoCollection.class);
+
+        when(mockDatabase.getCollection("Users")).thenReturn(mockCollection);
+    }
 
     @Test
     public void testAuthenticate_succes(){
--- src/test/java/com/mathapp/SignUpTest.java
+++ src/test/java/com/mathapp/SignUpTest.java
@@ -2,12 +2,27 @@ package com.mathapp;
 
 import java.util.logging.Logger;
 
+import org.bson.Document;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class SignUpTest{
     private static final Logger logger = Logger.getLogger(SignUpTest.class.getName());
+    private MongoDatabase mockDatabase;
+    private MongoCollection<Document> mockCollection;
+
+    public void setUp() {
+        mockDatabase = mock(MongoDatabase.class);
+        mockCollection = mock(MongoCollection.class);
+
+        when(mockDatabase.getCollection("users")).thenReturn(mockCollection);
+    }
+
     @Test
     public void testAuthenticate_succes(){
         logger.info("Start test.");
